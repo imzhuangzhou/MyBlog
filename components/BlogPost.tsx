a0e2f9995bb603182ce518e44bfa5c58ef1c900d
imzhuangzhou <imzhangjing@foxmail.com  git config --global user.name imzhuangzhou>
2025-08-01 21:51:45 -0700

    style(layout): Relocate post date to the right of the title

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -20,16 +20,18 @@ export const BlogPost: React.VFC<Props> = ({ post }) => {
         className="mt-2 mb-6 md:mb-8 hover:opacity-90 transition-transform ease-out hover:scale-105"
       >
         <header>
-          <time className="text-gray-600 dark:text-gray-400 text-sm mb-2 inline-block">
-            {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
-          </time>
-          <div className="flex">
-            <h2 className="mb-2 text-lg md:text-xl font-bold text-black dark:text-white cursor-pointer">
-              {post.title}
-            </h2>
-            {isOuterLink && (
-              <ArrowTopRightOnSquareIcon className="mt-[0.5px] mr-2 sm:mr-0 ml-1 w-5 min-w-[20px] h-5 text-blue-700 dark:text-blue-400" />
-            )}
+          <div className="flex justify-between items-start mb-2">
+            <div className="flex items-center flex-1">
+              <h2 className="text-lg md:text-xl font-bold text-black dark:text-white cursor-pointer">
+                {post.title}
+              </h2>
+              {isOuterLink && (
+                <ArrowTopRightOnSquareIcon className="mt-[0.5px] ml-1 w-5 min-w-[20px] h-5 text-blue-700 dark:text-blue-400" />
+              )}
+            </div>
+            <time className="text-gray-600 dark:text-gray-400 text-sm ml-4 flex-shrink-0">
+              {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
+            </time>
           </div>
         </header>
         {post?.thumbnail_url && (
